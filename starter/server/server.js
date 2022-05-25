4d70c3d95a74fec4d0d45d5f9c2ed89cbe559391
Keenan Faulkner <keenan.r.faulkner@gmail.com>
2022-05-25 11:55:34 -0600

    Handled post request on backend with pin encryption

---
+++ starter/server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const msgCtrl = require('./messageController');
+
+app.post('/api/messages', msgCtrl.createMessage);
+
+app.listen(4004, () => console.log("Listening on port 4004"));
